a54d084d62017b1bf4a864ed86023c5916abaf47
evanZccc <167677714+evanZccc@users.noreply.github.com>
2025-04-24 16:26:18 +0200

    Add files via upload

---
+++ UI/icons/ticketicons.js
@@ -0,0 +1,9 @@
+const ticketIcons = {
+    mainIcon: 'https://cdn.discordapp.com/attachments/1329962558744035348/1331604756661276714/200w.gif?ex=67923912&is=6790e792&hm=6f023d22d109b8ff7f1421f60afb49a3597bf55407d901dce6d1a592ade145d4&', 
+    correctIcon: 'https://cdn.discordapp.com/emojis/819446784647757834.gif', 
+    correctrIcon : 'https://cdn.discordapp.com/attachments/1230824451990622299/1236802049190920202/4104-verify-red.gif?ex=66a5702b&is=66a41eab&hm=5f38fab7b9dab73a6250db1a5e149b94bcdd49b19d6b70e38253fa2b2470615f&',
+    heartIcon: 'https://cdn.discordapp.com/attachments/1329962558744035348/1331603834451267604/verified.gif?ex=67923836&is=6790e6b6&hm=fa1e2ec80b6fb1c77b1d75479c3f1a808a56fd565add13a6a3fa7aa3153ad460&',
+    modIcon :'https://cdn.discordapp.com/emojis/805980519644004372.gif',
+};
+
+module.exports = ticketIcons;
